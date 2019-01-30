503a7260211e840157cc205bfc23b6c20d94baa3
Joe Pritchett and Matt Groberg <joe+matt@>
2019-01-30 13:52:19 -0800

    refactor trial

--- js/scripts.js
+++ js/scripts.js
@@ -8,72 +8,64 @@ var gameTracker=
   player2Array:[]
 }
 
-// function that generates random number and returns the number
-function changeTurn(turn){
-  if(turn==="p1"){
-    return "p2";
-  } else if (turn==="p1"){
-    return "p1";
+
+function rollDice(game){
+  var diceResult= Math.ceil(Math.random()*6);
+  if (diceResult===1){
+    clearArray(game.turn);
+    resetTurn(game.turn)
+  } else {
+    if (game.turn==="p1"){
+      game.player1Array.push(diceResult);
+    } else if (game.turn==="p2"){
+      game.player2Array.push(diceResult);
+    }
   }
+
 }
 
-function rollDice(turn){
-  var result = Math.ceil(Math.random()*6);
-  console.log("result is"+ result)
-  if(result!==1){
-    if (turn==="p1"){
-      gameTracker.player1Array.push(result);
-    }
-    if(turn==="p2"){
-      gameTracker.player2Array.push(result);
-    }
-  } else {
-    if (gameTracker.turn==="p1"){
-      clearArray("p1");
-    } else if (gameTracker.turn==="p2"){
-      clearArray("p2");
-    }
-  }
-}
 
 function clearArray(turn){
   if (turn==="p1"){
     gameTracker.player1Array=[];
-    gameTracker.turn="p2";
   } else if(turn==="p2") {
     gameTracker.player2Array=[];
-    gameTracker.turn="p1"
   }
 };
 
-function addScore(array){
-  var score = 0
-  array.forEach(function(roll){
-    score += roll;
-  });
-  if (gameTracker.turn === "p1") {
-    gameTracker.player1Score += score;
-    console.log("before p1")
-    clearArray("p1");
-    console.log("after p1")
-  } else if(gameTracker.turn==="p2") {
-    gameTracker.player2Score += score;
-    console.log("before p2")
-    clearArray("p2");
-    console.log("after p2")
+function resetTurn(turn){
+  if (turn==="p1"){
+    gameTracker.turn="p2";
+  } else if(turn==="p2") {
+    gameTracker.turn="p1"
   }
 }
 
+function addScore(turn){
+  var score = 0;
+  if (turn === "p1") {
+    gameTracker.player1Array.forEach(function(roll){
+      score += roll;
+  })
+  } else if (turn === "p2") {
+      gameTracker.player2Array.forEach(function(roll){
+        score += roll;
+    });
+  }
+}
+
+// function(){
+//   if (gameTracker.turn == "p1") {
+//     addScore(gameTracker.player1Array);
+//   }
+//   if (gameTracker.turn == "p2") {
+//     addScore(gameTracker.player2Array);
+//   }
+// }
+
+
+
 $(document).ready(function(){
-  $("#rollBtn").click(function(){
-    var diceResult= rollDice(gameTracker.turn);
-  })
-  $("#scoreBtn").click(function(){
-    if (gameTracker.turn == "p1") {
-      addScore(gameTracker.player1Array);
-    }
-    if (gameTracker.turn == "p2") {
-      addScore(gameTracker.player2Array);
-    }
-  })
+  $("#rollBtn").click(rollDice(gameTracker));
+  $("#scoreBtn").click(addScore(gameTracker.turn))
 });
