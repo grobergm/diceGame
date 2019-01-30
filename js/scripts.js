d1c41845a6b0928a5dc2c6e8b197a8d231d275fe
Joe Pritchett and Matt Groberg <matt@>
2019-01-30 14:29:48 -0800

    fix bug in turn rotation logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,52 @@
 
 var gameTracker=
 {
-  turn:"p1",
+  turn: 1,
   player1Score: 0,
-  player1Array:[],
   player2Score: 0,
-  player2Array:[]
+  rollArray:[]
 }
 
 // function that generates random number and returns the number
-function changeTurn(turn){
-  if(turn==="p1"){
-    return "p2";
-  } else if (turn==="p1"){
-    return "p1";
-  }
-}
+// function changeTurn(turn){
+//   if(turn==="p1"){
+//     return "p2";
+//   } else if (turn==="p1"){
+//     return "p1";
+//   }
+// }
 
 function rollDice(turn){
   var result = Math.ceil(Math.random()*6);
   console.log("result is"+ result)
   if(result!==1){
-    if (turn==="p1"){
-      gameTracker.player1Array.push(result);
-    }
-    if(turn==="p2"){
-      gameTracker.player2Array.push(result);
-    }
+    gameTracker.rollArray.push(result);
   } else {
-    if (gameTracker.turn==="p1"){
-      clearArray("p1");
-    } else if (gameTracker.turn==="p2"){
-      clearArray("p2");
-    }
+    gameTracker.turn += 1;
+    clearArray();
   }
+  console.log(gameTracker.turn);
 }
 
-function clearArray(turn){
-  if (turn==="p1"){
-    gameTracker.player1Array=[];
-    gameTracker.turn="p2";
-  } else if(turn==="p2") {
-    gameTracker.player2Array=[];
-    gameTracker.turn="p1"
-  }
+function clearArray(){
+  gameTracker.rollArray=[];
 };
 
 function addScore(array){
   var score = 0
   array.forEach(function(roll){
     score += roll;
+  console.log(array)
   });
-  if (gameTracker.turn === "p1") {
+  if (gameTracker.turn%2!==0) {
     gameTracker.player1Score += score;
-    console.log("before p1")
-    clearArray("p1");
-    console.log("after p1")
-  } else if(gameTracker.turn==="p2") {
+    console.log("p1")
+  } else if(gameTracker.turn%2===0) {
     gameTracker.player2Score += score;
-    console.log("before p2")
-    clearArray("p2");
-    console.log("after p2")
+    console.log("p2")
   }
+  gameTracker.turn += 1;
+  clearArray();
 }
 
 $(document).ready(function(){
@@ -69,11 +54,6 @@ $(document).ready(function(){
     var diceResult= rollDice(gameTracker.turn);
   })
   $("#scoreBtn").click(function(){
-    if (gameTracker.turn == "p1") {
-      addScore(gameTracker.player1Array);
-    }
-    if (gameTracker.turn == "p2") {
-      addScore(gameTracker.player2Array);
-    }
+    addScore(gameTracker.rollArray);
   })
 });
