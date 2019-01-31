6f1d1680904862c0470bc36ab4f5d12cd191308a
Joe Pritchett and Matt Groberg <joe@>
2019-01-31 15:38:40 -0800

    complete refactoring using object constructor

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,11 @@ function Game(){
   this.rollArray=[];
 }
 
+function Player(name){
+  this.name=name;
+  this.score=0;
+}
+
 Game.prototype.turnChanger=function(){
   this.rollArray=[];
   if(this.turn<this.playerArray.length){
@@ -34,130 +39,61 @@ Game.prototype.addScore=function(){
     roundScore+=roll;
   });
   this.playerArray[this.turn-1].score+=roundScore;
+  this.turnChanger();
 }
 
 Game.prototype.checkVictory=function(){
   this.playerArray.forEach(function(player){
     if(player.score>=100){
-      alert(player.name + "Is the Winner!");
+      alert(player.name + " Is the Winner!");
     }
   })
 }
 
-function Player(name){
-  this.name=name;
-  this.score=0;
-}
-
-// var newGame= new Game();
-// var newPlayer= new Player("Matt");
-// var newPlayer2= new Player("Joe");
-// var newPlayer3= new Player("Mark");
-//
-//
-// newGame.addPlayer(newPlayer);
-// newGame.addPlayer(newPlayer2);
-// newGame.addPlayer(newPlayer3);
-//
-//
-//
-// var gameTracker=
-// {
-//   turn: 1,
-//   player1Score: 0,
-//   player2Score: 0,
-//   rollArray:[]
-// }
-//
-// function rollDice(turn){
-//   var result = Math.ceil(Math.random()*6);
-//   console.log("result is"+ result)
-//   if(result!==1){
-//     gameTracker.rollArray.push(result);
-//   } else {
-//     gameTracker.turn += 1;
-//     clearArray();
-//   }
-//   return result;
-// }
-//
-// function clearArray(){
-//   gameTracker.rollArray=[];
-// };
-//
-// function addScore(array){
-//   var score = 0
-//   array.forEach(function(roll){
-//     score += roll;
-//   console.log(array)
-//   });
-//   if (gameTracker.turn%2!==0) {
-//     gameTracker.player1Score += score;
-//     console.log("p1")
-//   } else if(gameTracker.turn%2===0) {
-//     gameTracker.player2Score += score;
-//     if(gameTracker.player2Score>= 100){
-//      alert("Player 2 Wins!");
-//     }
-//     console.log("p2")
-//   }
-//   checkVictory();
-//   gameTracker.turn += 1;
-//   clearArray();
-// }
-//
-// function checkVictory(){
-//   if(gameTracker.player1Score>= 100){
-//      alert("Player 1 Wins!");
-//   }
-//   if(gameTracker.player2Score>=100){
-//     alert("Player 2 Wins!");
-//   }
-//   if ((gameTracker.player1Score>=100)||(gameTracker.player2Score>=100)){
-//     gameTracker.player1Score=0;
-//     gameTracker.player2Score=0;
-//   }
-// }
-//
 
 
 $(document).ready(function(){
   var newGame = new Game();
 
+  function displayPlayers(){
+    $(".player").hide();
+    newGame.playerArray.forEach(function(player){
+      var appendString="<div class='card player'><h3>"+player.name+"</h3><p>"+player.score+"</p></div>"
+      $("#playerDisplay").append(appendString);
+    })
+  };
+
+  function displayRoll(){
+    var rollString= newGame.rollArray.join(", ");
+    $("#rollDisplay").text(rollString);
+  };
+
+  function displayTurn(){
+    $("#turnDisplay").text(newGame.playerArray[newGame.turn-1].name)
+  };
+
   $('#nameForm').submit(function(event){
     event.preventDefault();
     var playerName= $("#nameInput").val();
-    newGame.addPlayer(playerName);
-    console.log(newGame)
+    if(playerName){
+      $("#nameInput").removeClass("is-invalid");
+      newGame.addPlayer(playerName);
+      displayPlayers();
+    } else {
+      $("#nameInput").addClass("is-invalid");
+    }
   });
 
   $("#startGame").click(function(){
     $("#titleDisplay").hide();
     $("#gameDisplay").show();
-  })
 
-
-  function displayPlayers(){
-    newGame.playerArray.forEach(function(player){
-      var appendString="<div class='card'><h3>"+player.name+"</h3><p>"+player.score+"</p></div>"
-      $("#p1Display").append(appendString)
-    })
-  };
-
-
-
-  function displayRoll(){
-    var rollString= gameTracker.rollArray.join(", ");
-    $("#rollDisplay").text(rollString);
-  };
-
-  function displayPlayerScore(){
-    $("#p1Score").text(gameTracker.player1Score);
-    $("#p2Score").text(gameTracker.player2Score);
-  }
+  });
   $("#rollBtn").click(function(){
-    var diceResult= rollDice(gameTracker.turn);
+    var diceResult= newGame.rollDice();
+    displayRoll();
     $("#diceDisplay img").hide();
+
     if(diceResult===1){
       $("#grin").show();
     }
@@ -177,14 +113,16 @@ $(document).ready(function(){
       $("#six").show();
     }
   });
-
   $("#scoreBtn").click(function(){
-    addScore(gameTracker.rollArray);
+    newGame.addScore();
+
+    $("#diceDisplay img").hide();
   })
   $("button").click(function(){
-    displayPlayerScore();
-    displayTurn();
     displayRoll();
+    displayPlayers();
+    newGame.checkVictory();
+    displayTurn();
   })
 
 });
